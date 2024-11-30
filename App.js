a7549d0f84ac40b410d190df24ea369d30cf7867
Arpittiwari1 <arpittiwari0408+1@gmail.com>
2024-11-30 23:01:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,95 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#Reset-btn");
+let newGamebtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turn0 = true; // player X , player Y
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],  
+];
+
+const resetGame = () => {
+    turn0 = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+    msgContainer.classList.remove("congratulations", "draw", "show");
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked");
+        if (turn0) {
+            box.innerText = "X";
+            turn0 = false;
+        } else {
+            box.innerText = "O";
+            turn0 = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    msgContainer.classList.add("show", "congratulations");
+    disableBoxes();
+}
+
+const showDraw = () => {
+    msg.innerText = "It's a draw!";
+    msgContainer.classList.remove("hide");
+    msgContainer.classList.add("show", "draw");
+    disableBoxes();
+}
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+        if (pos1val != "" && pos2val != "" && pos3val != "") {
+            if (pos1val === pos2val && pos2val === pos3val) {
+                showWinner(pos1val);
+                return;
+            }
+        }
+    }
+    checkDraw();
+}
+
+const checkDraw = () => {
+    for (let box of boxes) {
+        if (box.innerText === "") {
+            return; // There's still an empty box, so it's not a draw yet.
+        }
+    }
+    showDraw(); // All boxes are filled and there's no winner.
+}
+
+newGamebtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
